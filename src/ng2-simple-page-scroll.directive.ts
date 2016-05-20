1a62f020d649f7104f655dc530dea4d5610bd0b5
Benny Bottema <benny@bennybottema.com>
2016-05-20 20:00:00 +0000

    replace implementation with the simple version

--- src/ng2-simple-page-scroll.directive.ts
+++ src/ng2-simple-page-scroll.directive.ts
@@ -1,12 +1,12 @@
-import {Directive, ElementRef, Input, Output, EventEmitter, HostListener, OnDestroy} from '@angular/core';
+import {Directive, ElementRef, Input, Output, HostListener } from '@angular/core';
 import {Router} from '@angular/router';
 import {Subscription} from 'rxjs/Subscription';
-import {SimplePageScrollConfig, IEasingFunction} from './ng2-simple-page-scroll-config';
+import {SimplePageScrollConfig} from './ng2-simple-page-scroll-config';
 
 @Directive({
     selector: '[simplePageScroll]'
 })
-export class SimplePageScroll implements OnDestroy {
+export class SimplePageScroll {
 
     @Input()
     public routerLink:any;
@@ -17,37 +17,12 @@ export class SimplePageScroll implements OnDestroy {
     @Input()
     public simplePageScrollOffset:number = null;
 
-    @Input()
-    public simplePageScrollDuration:number = null;
-
-    @Input()
-    public simplePageScrollEasing:IEasingFunction = null;
-
-    @Output()
-    simplePageScrollFinish:EventEmitter<any> = new EventEmitter();
-
     private document:Document;
     private body:HTMLBodyElement;
-    private listener:EventListenerOrEventListenerObject = (event:Event):void => {
-        // Stop the scroll animation if the user interferes with it
-        if (event.type !== 'keyup' || SimplePageScroll.interfereKeys.indexOf((<KeyboardEvent>event).keyCode) >= 0) {
-            SimplePageScroll.stopTimers();
-        }
-    };
-
-    private static timers:any[] = [];
-    private static interfereEvents:string[] = ['mousedown', 'wheel', 'DOMMouseScroll', 'mousewheel', 'keyup', 'touchmove'];
-    private static interfereKeys:number[] = [33, 34, 35, 36, 38, 40];
 
     constructor(private el:ElementRef, private router:Router) {
         this.document = el.nativeElement.ownerDocument;
         this.body = el.nativeElement.ownerDocument.body;
-        SimplePageScroll.interfereEvents.forEach((event:string) => this.body.addEventListener(event, this.listener));
-    }
-
-    ngOnDestroy():any {
-        SimplePageScroll.interfereEvents.forEach((event:string) => this.body.removeEventListener(event, this.listener));
-        return undefined;
     }
 
     @HostListener('click', ['$event'])
@@ -73,58 +48,7 @@ export class SimplePageScroll implements OnDestroy {
         if (anchorTarget !== null) {
             let targetScrollTop:number = anchorTarget.offsetTop;
             let distanceToScroll:number = targetScrollTop - this.body.scrollTop;
-
-            if (distanceToScroll !== 0) {
-                SimplePageScroll.stopTimers();
-
-                let startTime:number = new Date().getTime();
-
-                let intervalConf:any = {
-                    startScrollTop: this.body.scrollTop,
-                    targetScrollTop: distanceToScroll -
-                    (this.simplePageScrollOffset === null ? SimplePageScrollConfig.defaultScrollOffset : this.simplePageScrollOffset),
-                    startTime: startTime,
-                    easing: this.simplePageScrollEasing === null ? SimplePageScrollConfig.defaultEasingFunction : this.simplePageScrollEasing
-                };
-                intervalConf.duration = this.simplePageScrollDuration === null ? SimplePageScrollConfig.defaultDuration : this.simplePageScrollDuration;
-                intervalConf.endTime = intervalConf.startTime + intervalConf.duration;
-
-                let timer:any = setInterval((intervalConf:any) => {
-                    let currentTime:number = new Date().getTime();
-                    this.body.scrollTop = intervalConf.easing(
-                        currentTime - intervalConf.startTime,
-                        intervalConf.startScrollTop,
-                        intervalConf.targetScrollTop,
-                        intervalConf.duration);
-
-                    if (intervalConf.endTime <= currentTime) {
-                        SimplePageScroll.stopTimer(timer);
-                        this.simplePageScrollFinish.emit(null);
-                    }
-                }, SimplePageScrollConfig._interval, intervalConf);
-                SimplePageScroll.timers.push(timer);
-            }
+            this.body.scrollTop = anchorTarget.offsetTop - anchorTarget.scrollTop + anchorTarget.clientTop;
         }
-    }
-
-    private static stopTimers():boolean {
-        if (SimplePageScroll.timers.length > 0) {
-            SimplePageScroll.timers.forEach((timer:any, index:number) => {
-                clearInterval(timer);
-                SimplePageScroll.timers.splice(index, 1);
-            });
-            return true;
-        }
-        return false;
-    }
-
-    private static stopTimer(timer:any):boolean {
-        clearInterval(timer);
-        let index:number = SimplePageScroll.timers.indexOf(timer);
-        if (index >= 0) {
-            SimplePageScroll.timers.splice(index, 1);
-            return true;
-        }
-        return false;
     }
 }
